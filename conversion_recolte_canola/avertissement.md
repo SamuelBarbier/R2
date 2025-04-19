08a198ae9b70bcb33600203e5eef4e5f074d490c
Amaury6 <amaury.travouillon@icloud.com>
2025-04-20 00:57:14 +0200

    avancement

---
+++ conversion_recolte_canola/avertissement.md
@@ -0,0 +1,3 @@
+# Avertissement
+
+**Ce guide ne s'applique qu'aux modèles ayant un numéro de série supérieur à 102000.**
---
+++ conversion_recolte_canola/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+var keywords=[{w:"Avertissement",p:["p0"]},{w:"Battage",p:["p1"]},{w:"Caisson",p:["p2","p12"]},{w:"de",p:["p2","p3","p7","p9","p12"]},{w:"nettoyage",p:["p2","p12"]},{w:"Configuration",p:["p3"]},{w:"la",p:["p3"]},{w:"plateforme",p:["p3"]},{w:"Contre-batteurs",p:["p4"]},{w:"sectionnels",p:["p4"]},{w:"Convoyeur",p:["p5"]},{w:"d\'alimentation",p:["p5"]},{w:"Context",p:["p6"]},{w:"Sensitive",p:["p6"]},{w:"Help",p:["p6"]},{w:"\u00C9parpilleur",p:["p7"]},{w:"menues",p:["p7"]},{w:"pailles",p:["p7"]},{w:":",p:["p7","p9"]},{w:"r\u00E9gime",p:["p7","p9"]},{w:"et",p:["p7"]},{w:"ailettes",p:["p7"]},{w:"Manutention",p:["p8"]},{w:"du",p:["p8","p9"]},{w:"grain",p:["p8","p12"]},{w:"Plage",p:["p9"]},{w:"batteur",p:["p9"]},{w:"positions",p:["p9"]},{w:"des",p:["p9"]},{w:"axes",p:["p9"]},{w:"R\u00E9sidus",p:["p10"]},{w:"S\u00E9paration",p:["p11"]},{w:"Type",p:["p12"]},{w:"-",p:["p12"]},{w:"grille",p:["p12"]},{w:"\u00E0",p:["p12"]},{w:"\u00F4tons",p:["p12"]},{w:"/",p:["p12"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2, 3, 4];
+ph["p3"]=[5, 3, 6, 7];
+ph["p4"]=[8, 9];
+ph["p5"]=[10, 11];
+ph["p6"]=[12, 13, 14];
+ph["p7"]=[15, 3, 16, 17, 18, 19, 20, 21];
+ph["p8"]=[22, 23, 24];
+ph["p9"]=[25, 3, 19, 23, 26, 18, 27, 28, 29];
+ph["p10"]=[30];
+ph["p12"]=[32, 3, 2, 3, 4, 33, 34, 35, 36, 37, 34, 35, 24];
+ph["p11"]=[31];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
